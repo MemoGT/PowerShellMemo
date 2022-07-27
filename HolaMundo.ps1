5177c673479736114e7a36719639350be5be6082
MemoGT <guillermo.sierra@gmail.com>
2022-07-27 10:07:25 -0600

    July 27 10:06AM - HolaMundo

--- HolaMundo.ps1
+++ HolaMundo.ps1
@@ -1,2 +1,5 @@
 write-host "Prueba 1"
-write-host "Prueba 2"
+#write-host "Prueba 2"
+#$PSVersionTable #Ver la version de powershell
+(Get-Command).count #Ver cuantos comandos estan cargados en el sistema
+get-command -Name *DHCP*    #Para ver todos los comandos que contengan DHCP
