466a9eb390b77115e34e33b9e233818723865153
MemoGT <guillermo.sierra@gmail.com>
2022-07-29 16:29:13 -0600

    Services stopped 4:28PM

--- ServicesStopped.ps1
+++ ServicesStopped.ps1
@@ -9,3 +9,7 @@ Select-Object Name, Status  # Seleccionar solo el nombre y status
 $services = Get-Service | Where-Object status -eq 'running' | Select-Object name, Status  # Mete en una variable todo el comando
 $services | Out-File C:\Users\A462138\Documents\PowerShellMemo\PowerShellMemo\services.csv  # Mete el resultado de la variable en un archivo de excel .csv
 notepad .\services.csv  # Abre el contenido del archivo en notepad
+
+$services | Export-Csv .\services2.csv # Exporta el contenido de la variable a un archivo csv
+get-content .\services2.csv | more # Abre el contenido del archivo en consola, esto solo para que se mire mas entendible
+
